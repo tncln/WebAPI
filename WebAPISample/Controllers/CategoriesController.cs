4ba9f8eecc48825f9dd6cef8e4ab0231e9913b3c
JobTracking <tuncalin@outlook.com>
2022-04-10 23:57:56 +0300

    CRUD İşlemleri

---
+++ WebAPISample/Controllers/CategoriesController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPISample.DAL.Context;
+using WebAPISample.DAL.Entities;
+
+namespace WebAPISample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using var context = new WebAPIContext();
+            return Ok(context.Categories.ToList());
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            using var context = new WebAPIContext();
+            var category = context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category);
+        }
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            using var context = new WebAPIContext();
+            var updatedCategory = context.Categories.Find(category.Id);
+            if (updatedCategory == null)
+            {
+                return NotFound();
+            }
+            updatedCategory.Name = category.Name;
+            context.Update(updatedCategory);
+            context.SaveChanges();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            using var context = new WebAPIContext();
+            var deletedCategory = context.Categories.Find(id);
+            if (deletedCategory == null)
+            {
+                return NotFound();
+            }
+            context.Remove(deletedCategory);
+            context.SaveChanges();
+            return NoContent();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            using var context = new WebAPIContext();
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return Created("", category);
+        }
+        [HttpGet("{id}/blogs")]
+        public IActionResult GetWithBlogsById(int id)
+        {
+            using var context = new WebAPIContext();
+            var category = context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            var categoryWithBlogs= context.Categories.Where(x => x.Id == id).Include(I => I.Blogs).ToList();
+            return Ok(categoryWithBlogs);
+        }
+    }
+}
---
+++ WebAPISample/DAL/Context/WebAPIContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPISample.DAL.Entities;
+
+namespace WebAPISample.DAL.Context
+{
+    public class WebAPIContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.; database=WebAPISample; integrated security=true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
